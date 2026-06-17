2cd20b5e76ee7d7e841d391b5bdca57783cdb46e
salimbil <salimbilalov7@gmail.com>
2026-06-17 22:10:07 +0500

    Create A.py

---
+++ 7_greedy/ 7.1_change/A.py
@@ -0,0 +1,15 @@
+# Компилятор: Python 3.11
+
+final = []
+cnt = 0
+n = int(input())
+
+for ten in range((n // 10) + 1):
+        
+    for five in range(((n - (10 * ten)) // 5) + 1):
+        one = n - 10 * ten - 5 * five
+        steps = str(ten + five + one)        
+        final.append((steps + ' ' + '10 ' * ten + '5 ' * five + '1 ' * one).rstrip())
+        cnt += 1
+print(cnt)
+print(*final, sep='\n')
