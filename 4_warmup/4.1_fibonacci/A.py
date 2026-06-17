a401996aa91b4dd85bd1337696590525e21fd40f
salimbil <salimbilalov7@gmail.com>
2026-06-17 21:11:56 +0500

    Create A.py

---
+++ 4_warmup/4.1_fibonacci/A.py
@@ -0,0 +1,12 @@
+# Компилятор: Python 3.11
+
+def fib(a):
+    if n <= 1:
+        return n
+    f_0, f_1 = 0, 1
+    for i in range(2, a+1):
+        f_0, f_1 = f_1, f_0 + f_1
+    return f_1
+
+n = int(input())
+print(fib(n))
