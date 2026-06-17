bb0a120a90472615852dc74e681b90a39c1bc19b
salimbil <salimbilalov7@gmail.com>
2026-06-17 22:10:54 +0500

    Create B.py

---
+++ 7_greedy/ 7.1_change/B.py
@@ -0,0 +1,16 @@
+# Компилятор: Python 3.11
+
+n = int(input())
+nominal = [1, 5, 10, 20, 50]
+
+numCoins = 0
+final = []
+
+for i in nominal[::-1]:
+    if n >= i:
+        numCoins += n // i
+        final += [i] * (n // i)
+        n = n % i
+
+print(numCoins)
+print(*final)
