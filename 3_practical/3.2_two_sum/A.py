e90de630cf1624067bb7033b922478675d691102
salimbil <salimbilalov7@gmail.com>
2026-06-17 20:59:46 +0500

    Create A.py

---
+++ 3_practical/3.2_two_sum/A.py
@@ -0,0 +1,8 @@
+# Компилятор: Python 3.13.2
+
+def sum_of_digits(first_digit, second_digit):
+    return first_digit + second_digit
+ 
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(sum_of_digits(a, b))
