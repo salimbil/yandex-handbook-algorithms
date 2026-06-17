c77006f7ea61cda9131f9357b5f512ccb23e45b3
salimbil <salimbilalov7@gmail.com>
2026-06-17 21:10:02 +0500

    Create D.py

---
+++ 3_practical/3.3_max_product/D.py
@@ -0,0 +1,12 @@
+# Компилятор: Python 3.11
+
+def max_pair(arr):
+    arr.sort()
+    right = arr[-2] * arr[-1] * arr[-3] * arr[-4]
+    left = arr[0] * arr[1] * arr[-1] * arr[-2]
+    full_left = arr[0] * arr[1] * arr[2] * arr[3]
+    print(max(right, left, full_left))
+
+n = int(input())
+m = list(map(int, input().split()))
+max_pair(m)
