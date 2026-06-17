aff46f2ed39db54c93638adb167680ef520a5c84
salimbil <salimbilalov7@gmail.com>
2026-06-17 21:00:59 +0500

    Create B.py

---
+++ 3_practical/3.2_two_sum/B.py
@@ -0,0 +1,18 @@
+# Компилятор: Python 3.13.2
+
+def many(a_x, b_x, arr_a, arr_b):
+    print(max(a_x, b_x))
+    ind = -1
+
+    for i in arr_b[::-1]:
+        arr_a[ind] += i
+        ind -= 1
+    print(*arr_a)        
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+if len(a) != len(b):
+    a, b = max(a, b, key=len), min(a, b, key=len)
+many(n, m, a, b)
