bd49c4327f71d185b4216d500cf19cd24fb7a6d9
salimbil <salimbilalov7@gmail.com>
2026-06-17 22:03:58 +0500

    Create D.py

---
+++ 6_techniques/ 6.5_divide_conquer/D.py
@@ -0,0 +1,12 @@
+# Компилятор: Python 3.11
+n = int(input())
+a = list(map(int, input().split()))
+
+max_sum = a[0]
+current_sum = a[0]
+
+for i in range(1, n):
+    current_sum = max(a[i], current_sum + a[i])
+    max_sum = max(max_sum, current_sum)
+
+print(max_sum)
