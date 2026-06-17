068e55adc2db6d1c9a40896a3da4c544a694b9f9
salimbil <salimbilalov7@gmail.com>
2026-06-17 21:02:00 +0500

    Create D.py

---
+++ 3_practical/3.2_two_sum/D.py
@@ -0,0 +1,11 @@
+# Компилятор: Python 3.13.2
+
+n, m = map(int, input().split())
+matrix_f = [list(map(int, input().split())) for k in range(n)]
+matrix_s = [list(map(int, input().split())) for k in range(n)]
+
+res = [[matrix_f[j][i] + matrix_s[j][i] for i in range(m)] for j in range(n)]
+
+for x in range(n):
+    for y in range(m):
+        print(res[x][y], end=' ')
