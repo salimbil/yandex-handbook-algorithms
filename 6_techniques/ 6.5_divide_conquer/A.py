3eb06828de545287fe81b171f6f2a84d97b50887
salimbil <salimbilalov7@gmail.com>
2026-06-17 22:01:52 +0500

    Create A.py

---
+++ 6_techniques/ 6.5_divide_conquer/A.py
@@ -0,0 +1,15 @@
+# Компилятор: Python 3.11
+
+def select_sort(seq):
+    for i in range(len(seq) - 1):
+        min_index = i
+        for k in range(i + 1, len(seq)):
+            if seq[k] < seq[min_index]:
+                min_index = k
+        seq[i], seq[min_index] = seq[min_index], seq[i]
+    return seq
+
+n = int(input())
+a = list(map(int, input().split()))
+
+print(*select_sort(a))
